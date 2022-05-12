47d7052b42377a6242fc94114561cc5104f5a86e
Hala Emad <35690579+HalaEH@users.noreply.github.com>
2022-05-12 07:43:01 -0700

    Init

---
+++ SportsApp/SportsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SportsApp
+//
+//  Created by Hala Emad on 5/12/22.
+//  Copyright © 2022 Hala Emad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
