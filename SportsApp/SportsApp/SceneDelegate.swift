778930a7305685c076694e0e4fed255329bee449
Muhammad Kholif <66799646+muhammadkh242@users.noreply.github.com>
2022-05-13 06:18:01 -0700

    Fix Main.StoryBoard Issue

--- SportsApp/SportsApp/SceneDelegate.swift
+++ SportsApp/SportsApp/SceneDelegate.swift
@@ -24,15 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, context)
-
-        // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+      guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ SportsApp/SportsApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Muhammad Kholif on 5/13/22.
+//  Copyright © 2022 Hala Emad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func btn(_ sender: UIButton) {
+        print("hi")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
