63a9d9e9b4bd452d6d080e02221560eb99f9c150
Hala Emad <35690579+HalaEH@users.noreply.github.com>
2022-05-15 09:16:24 -0700

    AllSports

--- SportsApp/SportsApp/Module/AllSports/Presenter/AllSportsPresenter.swift
+++ SportsApp/SportsApp/Module/AllSports/Presenter/AllSportsPresenter.swift
@@ -8,4 +8,35 @@
 
 import Foundation
 
+class AllSportsPresenter {
+    
+    var result : [Sport]! // model
+    weak var view : AllSportsProtocol!  // DI
+    
+    init(NWService : NetworkService){
+        //self.NWService = NWService
+        //NWService = NetworkService() // no Dependency Injection
+    }
+    
+    func attachView(view: AllSportsProtocol){
+        self.view = view
+    }
+    
+    func getItems(){
+        NetworkService.getAllSports{[weak self ](result ,error) in
+            if result?.sports != nil {
+                self?.result = result?.sports
+                print(result?.sports[0].sportName ?? "")
+            }
+            else {
+                print("error")
+            }
+            DispatchQueue.main.async {
+                self?.view.stopAnimating()
+                self?.view.updateUI(sports: result!.sports)
+            }
+        }
+    }
+    
+}
 
